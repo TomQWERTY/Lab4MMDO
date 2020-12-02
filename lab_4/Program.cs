539f71d942e92c846ceafcfc721a382b7999f24d
Olexandr Korkots <k.sasha.g@gmail.com>
2020-12-02 20:25:25 +0200

    Changed work of range in ToString and Main

--- lab_4/Program.cs
+++ lab_4/Program.cs
@@ -10,26 +10,24 @@ namespace lab_4
     delegate double[] Method(double[] range1, double e, Function F);
     class Program
     {
-        static string ToString(Method M, double[] range1, double e, Function F)
+        static string ToString(Method M, double[] range, double e, Function F)
         {
             long time = DateTime.Now.Ticks;
-            double[] res = M(range1, e, F);
+            double[] range_ = new double[2];
+            Array.Copy(range, range_, 2);
+            double[] res = M(range_, e, F);
             return (DateTime.Now.Ticks - time).ToString() + "\t" + res[0] + "\t" + res[1];
         }
         static void Main(string[] args)
         {
-            double[] range = new double[2] { 1, 2 },  range_ = new double[2];
+            double[] range = new double[2] { 1, 2 };
             double e = Math.Pow(10, -4);
             Function F = x => Math.Pow(x, 4) + 4 * Math.Pow(x, 3) - 3 * Math.Pow(x, 2) - 36 * x + 45;
             Console.WriteLine(ToString(LibraryOfMethods.Local, new double[2] { -100, 100 }, e, F));
-            Array.Copy(range, range_, 2);
-            Console.WriteLine(ToString(LibraryOfMethods.Duhot, range_, e, F));
-            Array.Copy(range, range_, 2);
-            Console.WriteLine(ToString(LibraryOfMethods.Gold, range_, e, F));
-            Array.Copy(range, range_, 2);
-            Console.WriteLine(ToString(LibraryOfMethods.Fibonacci, range_, e, F));
-            Array.Copy(range, range_, 2);
-            Console.WriteLine(ToString(LibraryOfMethods.Parabola, range_, e, F));
+            Console.WriteLine(ToString(LibraryOfMethods.Duhot, range, e, F));
+            Console.WriteLine(ToString(LibraryOfMethods.Gold, range, e, F));
+            Console.WriteLine(ToString(LibraryOfMethods.Fibonacci, range, e, F));
+            Console.WriteLine(ToString(LibraryOfMethods.Parabola, range, e, F));
             Console.ReadKey();
         }
     }
